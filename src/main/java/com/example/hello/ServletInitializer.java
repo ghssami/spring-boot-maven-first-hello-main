8835ea756b5f8e524dda14c2c1c71852f744cf5c
ghssami <ggwuridl@gmail.com>
2026-07-29 13:45:09 +0900

    [NullMOD]

--- src/main/java/com/example/hello/ServletInitializer.java
+++ src/main/java/com/example/hello/ServletInitializer.java
@@ -6,6 +6,6 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 public class ServletInitializer extends SpringBootServletInitializer {
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(SimpleHelloApplication.class);
+        return application.sources(SimpleHelloApplication.class); 
     }
 }
